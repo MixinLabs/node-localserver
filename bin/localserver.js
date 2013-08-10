7cb1693b9299bc7776ba9d748e52a0489bb324d3
Arturas Lebedevas <nsmeta@gmail.com>
2013-08-11 00:03:21 +0100

    better support for defaults

--- bin/localserver.js
+++ bin/localserver.js
@@ -3,13 +3,18 @@
 (function () {
     "use strict";
     
-    var program     = require('commander');
-    var localserver = require('../lib/server');
+    var program     = require('commander'),
+        _           = require('underscore'),
+        localserver = require('../lib/server'),
+        defaults    = localserver.defaults;
+    
+    // Mixin underscore.string
+    _.mixin(require('underscore.string').exports());
     
     program
         .version('0.1.0')
-        .option('-p, --port [port]', 'Start static server on specified port, defaults to "9000"', 9000)
-        .option('--index [index]', 'Specify index file, defaults to "index.html"', 'index.html')
+        .option('-p, --port [port]', _.sprintf('Start static server on specified port, defaults to "%d"', defaults.port), defaults.port)
+        .option('--index [index]', _.sprintf('Specify index file, defaults to "%s"', defaults.index), defaults.index)
         .parse(process.argv);
         
     localserver.startServer({
