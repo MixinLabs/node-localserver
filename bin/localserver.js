c4038bf72242ccd02ac54e1873610cad2c29395b
Arturas Lebedevas <nsmeta@gmail.com>
2013-08-11 13:47:24 +0100

    basic static http server functionality

--- bin/localserver.js
+++ bin/localserver.js
@@ -6,20 +6,29 @@
     var program     = require('commander'),
         _           = require('underscore'),
         localserver = require('../lib/server'),
-        defaults    = localserver.defaults;
+        defaults    = localserver.defaults,
+        opts;
     
     // Mixin underscore.string
     _.mixin(require('underscore.string').exports());
     
     program
         .version('0.1.0')
-        .option('-p, --port [port]', _.sprintf('Start static server on specified port, defaults to "%d"', defaults.port), defaults.port)
-        .option('--index [index]', _.sprintf('Specify index file, defaults to "%s"', defaults.index), defaults.index)
+        .option('-p, --port [port]', _.sprintf('Start static server on specified port. Defaults to "%d"', defaults.port), defaults.port)
+        .option('--index [index]', _.sprintf('Specify index file. Defaults to "%s"', defaults.index), defaults.index)
+        .option('--favicon [favicon]', 'If specified, will log a request to "/favicon.ico".')
         .parse(process.argv);
-        
-    new localserver.LocalServer({
+    
+    opts = {
         port: program.port,
-        index: program.index
-    });
+        index: program.index,
+        ignoreURLs: defaults.ignoreURLs
+    };
+    
+    if (program.favicon) {
+        opts.ignoreURLs = _.without(opts.ignoreURLs, '/favicon.ico');
+    }
+    
+    return new localserver.LocalServer(opts);
     
 }());
